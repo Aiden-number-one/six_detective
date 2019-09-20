ad9d4e635b57d05a389d46965adbeac132455582
mus <mus@szkingdom.com>
2019-09-20 10:16:17 +0800

    提交modal

--- src/models/global.js
+++ src/models/global.js
@@ -2,79 +2,16 @@
  * @Description: lan
  * @Author: lan
  * @Date: 2019-08-28 10:01:59
- * @LastEditTime: 2019-08-29 16:35:12
- * @LastEditors: lan
+ * @LastEditTime: 2019-09-20 10:14:51
+ * @LastEditors: mus
  */
-import { queryNotices } from '@/services/user';
-
 const global = {
   namespace: 'global',
   state: {
     notices: [],
     collapsed: false,
   },
-  effects: {
-    *fetchNotices(_, { call, put, select }) {
-      const data = yield call(queryNotices);
-      yield put({
-        type: 'saveNotices',
-        payload: data,
-      });
-      const unreadCount = yield select(
-        state => state.global.notices.filter(item => !item.read).length,
-      );
-      yield put({
-        type: 'user/changeNotifyCount',
-        payload: {
-          totalCount: data.length,
-          unreadCount,
-        },
-      });
-    },
-
-    *clearNotices({ payload }, { put, select }) {
-      yield put({
-        type: 'saveClearedNotices',
-        payload,
-      });
-      const count = yield select(state => state.global.notices.length);
-      const unreadCount = yield select(
-        state => state.global.notices.filter(item => !item.read).length,
-      );
-      yield put({
-        type: 'user/changeNotifyCount',
-        payload: {
-          totalCount: count,
-          unreadCount,
-        },
-      });
-    },
-
-    *changeNoticeReadState({ payload }, { put, select }) {
-      const notices = yield select(state =>
-        state.global.notices.map(item => {
-          const notice = { ...item };
-
-          if (notice.id === payload) {
-            notice.read = true;
-          }
-
-          return notice;
-        }),
-      );
-      yield put({
-        type: 'saveNotices',
-        payload: notices,
-      });
-      yield put({
-        type: 'user/changeNotifyCount',
-        payload: {
-          totalCount: notices.length,
-          unreadCount: notices.filter(item => !item.read).length,
-        },
-      });
-    },
-  },
+  effects: {},
   reducers: {
     changeLayoutCollapsed(state, { payload }) {
       return { ...state, collapsed: payload };
--- src/models/user.js
+++ src/models/user.js
@@ -2,33 +2,15 @@
  * @Description: user modal
  * @Author: lan
  * @Date: 2019-08-28 10:01:59
- * @LastEditTime: 2019-09-19 17:03:15
+ * @LastEditTime: 2019-09-20 10:13:57
  * @LastEditors: mus
  */
-import { queryCurrent, query as queryUsers } from '@/services/user';
-
 const user = {
   namespace: 'user',
   state: {
     currentUser: {},
   },
-  effects: {
-    *fetch(_, { call, put }) {
-      const response = yield call(queryUsers);
-      yield put({
-        type: 'save',
-        payload: response,
-      });
-    },
-
-    *fetchCurrent(_, { call, put }) {
-      const response = yield call(queryCurrent);
-      yield put({
-        type: 'saveCurrentUser',
-        payload: response,
-      });
-    },
-  },
+  effects: {},
   reducers: {
     saveCurrentUser(state, action) {
       return { ...state, currentUser: action.payload || {} };
