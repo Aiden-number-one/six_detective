af56e4efb98dc7d6f2e0c86d3341f320033cf47d
lanjianyan@szkingdom.com <lanjianyan@szkingdom.com>
2019-10-29 09:29:21 +0800

    提交表单页面

--- src/pages/FormItem/FormItem.js
+++ src/pages/FormItem/FormItem.js
@@ -2,24 +2,46 @@
  * @Description: 表单控件
  * @Author: lan
  * @Date: 2019-10-25 13:42:35
- * @LastEditTime: 2019-10-28 14:12:42
+ * @LastEditTime: 2019-10-28 18:36:05
  * @LastEditors: lan
  */
 import React, { Component, Fragment } from 'react';
-import { Form, Button, Input, Checkbox, Select, DatePicker, LocaleProvider } from 'antd';
+import {
+  Form,
+  Button,
+  Input,
+  Checkbox,
+  Select,
+  DatePicker,
+  LocaleProvider,
+  Row,
+  Col,
+  Radio,
+} from 'antd';
 import styles from './FormItem.less';
 
 const { TextArea } = Input;
 const { Option } = Select;
 
+// const formItemLayout = {
+//   labelCol: {
+//     xs: { span: 24 },
+//     sm: { span: 8 },
+//   },
+//   wrapperCol: {
+//     xs: { span: 24 },
+//     sm: { span: 16 },
+//   },
+// };
+
 const formItemLayout = {
   labelCol: {
     xs: { span: 24 },
-    sm: { span: 8 },
+    md: { span: 10 },
   },
   wrapperCol: {
     xs: { span: 24 },
-    sm: { span: 16 },
+    md: { span: 14 },
   },
 };
 
@@ -48,7 +70,134 @@ export default class FormItem extends Component {
     return (
       <Fragment>
         <Form className={styles['form-item']} {...formItemLayout}>
-          <Form.Item wrapperCol={{ offset: 8 }}>
+          <Row>
+            <Col xs={24} md={12}>
+              <Form.Item label="Trade Data" required>
+                {getFieldDecorator('tradeData', {
+                  rules: [{ required: true, message: 'Error' }],
+                  initialValue: undefined,
+                })(
+                  <LocaleProvider locale="en-us">
+                    <DatePicker style={{ width: '100%' }} />
+                  </LocaleProvider>,
+                )}
+              </Form.Item>
+            </Col>
+            <Col xs={24} md={12}>
+              <Form.Item label="EP Code">
+                {getFieldDecorator('epCode', {
+                  rules: [{ required: true, message: 'Error' }],
+                  initialValue: undefined,
+                })(<Input placeholder="Type your answer here" />)}
+              </Form.Item>
+            </Col>
+            <Col xs={24} md={12}>
+              <Form.Item label="Submitter Code">
+                {getFieldDecorator('submitterCode', {
+                  rules: [{ required: true, message: 'Error' }],
+                  initialValue: undefined,
+                })(<Input placeholder="Type your answer here" />)}
+              </Form.Item>
+            </Col>
+            <Col xs={24} md={12}>
+              <Form.Item label="Submitter Name">
+                {getFieldDecorator('submitterName', {
+                  rules: [{ required: true, message: 'Error' }],
+                  initialValue: undefined,
+                })(<Input placeholder="Type your answer here" />)}
+              </Form.Item>
+            </Col>
+            <Col xs={24} md={12}>
+              <Form.Item label="TO Code">
+                {getFieldDecorator('toCode', {
+                  rules: [{ required: true, message: 'Error' }],
+                  initialValue: undefined,
+                })(<Input placeholder="Type your answer here" />)}
+              </Form.Item>
+            </Col>
+            <Col xs={24} md={12}>
+              <Form.Item label="TO Name">
+                {getFieldDecorator('toName', {
+                  rules: [{ required: true, message: 'Error' }],
+                  initialValue: undefined,
+                })(<Input placeholder="Type your answer here" />)}
+              </Form.Item>
+            </Col>
+            <Col xs={24} md={12}>
+              <Form.Item label="LOP A/C NO.">
+                {getFieldDecorator('phoneNO', {
+                  rules: [{ required: true, message: 'Error' }],
+                  initialValue: undefined,
+                })(<Input placeholder="Type your answer here" />)}
+              </Form.Item>
+            </Col>
+            <Col xs={24} md={12}>
+              <Form.Item label="A/C Name">
+                {getFieldDecorator('acName', {
+                  rules: [{ required: true, message: 'Error' }],
+                  initialValue: undefined,
+                })(<Input placeholder="Type your answer here" />)}
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Form.Item label="Reported BI Name" labelCol={{ md: 5 }}>
+                {getFieldDecorator('RBIName', {
+                  rules: [{ required: true, message: 'Error' }],
+                  initialValue: undefined,
+                })(<Input placeholder="Type your answer here" />)}
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Form.Item label="Reported TO Name" labelCol={{ md: 5 }}>
+                {getFieldDecorator('RTOName', {
+                  rules: [{ required: true, message: 'Error' }],
+                  initialValue: undefined,
+                })(<Input placeholder="Type your answer here" />)}
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Form.Item label="Type of Account" labelCol={{ md: 5 }}>
+                {getFieldDecorator('accountType', {
+                  rules: [{ required: true, message: 'Error' }],
+                  initialValue: 'house',
+                })(
+                  <Radio.Group>
+                    <Radio value="house">House</Radio>
+                    <Radio value="client">Cilent</Radio>
+                  </Radio.Group>,
+                )}
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Form.Item label="Nature of Account" labelCol={{ md: 5 }}>
+                {getFieldDecorator('accountNature', {
+                  rules: [{ required: false }],
+                  initialValue: undefined,
+                })(
+                  <Checkbox.Group>
+                    <Checkbox onChange={() => {}} value="Hedge">
+                      Hedge
+                    </Checkbox>
+                    <Checkbox onChange={() => {}} value="Trading">
+                      Trading
+                    </Checkbox>
+                    <Checkbox onChange={() => {}} value="Arbitrage">
+                      Arbitrage
+                    </Checkbox>
+                  </Checkbox.Group>,
+                )}
+              </Form.Item>
+            </Col>
+          </Row>
+          {/* <Form.Item wrapperCol={{ offset: 8 }}>
             <Button>CANCEL</Button>
             <span> </span>
             <Button type="primary">SEARCH</Button>
@@ -56,7 +205,6 @@ export default class FormItem extends Component {
             <Button className="Add" icon="plus">
               New Data
             </Button>
-            <span> </span>
           </Form.Item>
           <Form.Item label="CheckBox">
             {getFieldDecorator('CheckBox', {
@@ -85,12 +233,12 @@ export default class FormItem extends Component {
               initialValue: undefined,
             })(<Input placeholder="Type Here" style={{ width: 200 }} />)}
           </Form.Item>
-          {/* <Form.Item label="Approved Text">
+          <Form.Item label="Approved Text">
             {getFieldDecorator('Input', {
               rules: [{ required: true, message: 'Error' }],
               initialValue: undefined,
             })(<Input placeholder="Type your answer here" style={{ width: 200 }} />)}
-          </Form.Item> */}
+          </Form.Item>
           <Form.Item label="Approved Sample" validateStatus="success" help="Approved">
             <Input
               placeholder="Type your answer here"
@@ -118,7 +266,7 @@ export default class FormItem extends Component {
               <DatePicker style={{ width: 200 }} />
             </LocaleProvider>
           </Form.Item>
-          <Form.Item></Form.Item>
+          <Form.Item></Form.Item> */}
         </Form>
       </Fragment>
     );
