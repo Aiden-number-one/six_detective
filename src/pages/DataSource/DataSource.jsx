d39e51c6e2e639ba4e582c55b8791193eec495f0
lanjianyan@szkingdom.com <lanjianyan@szkingdom.com>
2019-11-05 19:15:15 +0800

    tijiao

--- src/pages/DataSource/DataSource.jsx
+++ src/pages/DataSource/DataSource.jsx
@@ -332,6 +332,7 @@ export default class DataSource extends PureComponent {
               total: 12,
               size: 'small',
             }}
+            scroll={{ x: 'max-content' }}
           />
         </div>
       </PageHeaderWrapper>
