c59f132e2f65f3cf101b627d08ac5836541791f0
dailinbo <dailinbo@szkingdom.com>
2019-11-28 14:52:54 +0800

    userManagement

--- src/locales/en-US/contents/systemManagement.js
+++ src/locales/en-US/contents/systemManagement.js
@@ -3,13 +3,14 @@
  * @Author: dailinbo
  * @Date: 2019-11-21 16:17:40
  * @LastEditors: dailinbo
- * @LastEditTime: 2019-11-25 10:22:16
+ * @LastEditTime: 2019-11-28 14:21:45
  */
 export default {
   'systemManagement.userMaintenance.userId': 'User ID',
   'systemManagement.userMaintenance.userName': 'User Name',
-  'systemManagement.userMaintenance.lockedStatus': 'Locked Status',
+  'systemManagement.userMaintenance.lockedStatus': 'Locked',
   'systemManagement.userMaintenance.LastUpdateTime': 'Last Update Time',
+  'systemManagement.userMaintenance.LastUpdateUser': 'Last Update User',
   'systemManagement.emailParameter.severIP': 'Sever IP',
   'systemManagement.emailParameter.port': 'Port',
   'systemManagement.emailParameter.senderEmailAddress': 'Sender email address',
