2f3d9bfc2db9fce0cb187640065fd951a2e1f73d
dailinbo <dailinbo@szkingdom.com>
2019-11-29 11:30:42 +0800

    merge code

--- src/pages/DatasetManagement/DatasetManagement.jsx
+++ src/pages/DatasetManagement/DatasetManagement.jsx
@@ -1,18 +1,20 @@
 import React, { PureComponent } from 'react';
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
 import { connect } from 'dva';
-import // Tree,
-'antd';
+import { Icon, Button, Table, Input } from 'antd';
 import ClassifyTree from '@/components/ClassifyTree';
-// import styles from './DatasetManagement.less';
+import styles from './DatasetManagement.less';
 
-// const { TreeNode } = Tree;
+const { Search } = Input;
 
 @connect(({ dataSet }) => ({
   classifyTreeData: dataSet.classifyTreeData,
+  dataSetData: dataSet.dataSetData,
 }))
 export default class DatasetManagement extends PureComponent {
-  state = {};
+  state = {
+    selectedRowKeys: [],
+  };
 
   componentDidMount() {
     const { dispatch } = this.props;
@@ -21,24 +23,154 @@ export default class DatasetManagement extends PureComponent {
     });
   }
 
+  // 数据源列表标题
+  Title = () => (
+    <span className={styles.titleBox}>
+      <span>Data Connected</span>
+      <Icon
+        type="plus"
+        onClick={() => {
+          this.toggleModal('dataSourceType', 'ADD');
+        }}
+      />
+    </span>
+  );
+
   onSelect = value => {
+    const { dispatch } = this.props;
+    dispatch({
+      type: 'dataSet/getDataSet',
+      payload: {
+        taskId: value,
+        isShare: 0,
+      },
+    });
     console.log('value=', value);
   };
 
   render() {
-    const { classifyTreeData } = this.props;
-    console.log('classifyTreeData=', classifyTreeData);
+    // 表格表头
+    const columns = [
+      {
+        title: 'Name',
+        dataIndex: 'tableName',
+        key: 'tableName',
+        // sorter: (a, b) => a.name[1].length - b.name[1].length,
+      },
+      {
+        title: 'Type',
+        dataIndex: 'mdType',
+        key: 'mdType',
+        // sorter: (a, b) => a.name[1].length - b.name[1].length,
+      },
+      {
+        title: 'Data Source',
+        dataIndex: 'tableCat',
+        key: 'tableCat',
+        // sorter: (a, b) => a.name[1].length - b.name[1].length,
+      },
+      {
+        title: 'Creat by',
+        dataIndex: 'schemName',
+        key: 'schemName',
+        // sorter: (a, b) => a.name[1].length - b.name[1].length,
+      },
+      {
+        title: 'Modified by',
+        dataIndex: 'recordCount',
+        key: 'recordCount',
+        render: text => <span style={{ color: '#6ed2cb' }}>{text}</span>,
+      },
+      {
+        title: 'Modified at',
+        dataIndex: 'tableDesc',
+        key: 'tableDesc',
+      },
+      {
+        title: 'Action',
+        key: 'action',
+        render: (text, record) => (
+          <span>
+            <Icon
+              type="eye"
+              onClick={() => {
+                this.showDetail(record);
+              }}
+            />
+          </span>
+        ),
+      },
+    ];
+    const { selectedRowKeys } = this.state;
+    // 表格多选框
+    const rowSelection = {
+      selectedRowKeys,
+      type: 'checkbox',
+      onSelect: (record, selected, selectedRows) => {
+        const tableIds = selectedRows.map(item => item.tableId);
+        this.tableIds = tableIds.join(',');
+      },
+      onChange: selectedRowKey => {
+        this.setState({
+          selectedRowKeys: selectedRowKey,
+        });
+      },
+    };
+    const { classifyTreeData, dataSetData } = this.props;
     return (
       <PageHeaderWrapper>
-        <div>
-          <ClassifyTree
-            treeData={classifyTreeData}
-            treeKey={{
-              currentKey: 'classId',
-              currentName: 'className',
-            }}
-            onSelect={this.onSelect}
-          />
+        <div style={{ display: 'flex', minHeight: 500 }}>
+          <div style={{ flex: '0 0 220px', background: '#fff' }}>
+            {this.Title()}
+            <ClassifyTree
+              treeData={classifyTreeData}
+              treeKey={{
+                currentKey: 'classId',
+                currentName: 'className',
+              }}
+              onSelect={this.onSelect}
+              showSearch={false}
+            />
+          </div>
+          <div style={{ flex: 1, overflowX: 'auto' }}>
+            <div style={{ height: '100%', padding: '0 0 0 15px' }}>
+              <div
+                style={{
+                  display: 'flex',
+                  justifyContent: 'space-between',
+                  padding: '10px 5px 0',
+                  backgroundColor: '#fff',
+                }}
+              >
+                <Button
+                  style={{ width: 120 }}
+                  type="primary"
+                  onClick={() => {
+                    this.updMetadataOrColumn('some', 'updMetadata');
+                  }}
+                >
+                  Creat DataSet
+                </Button>
+                <Search
+                  style={{ width: 220 }}
+                  onSearch={value => {
+                    this.getDataSourceList(value);
+                  }}
+                />
+              </div>
+              <Table
+                className="basicTable"
+                rowSelection={rowSelection}
+                columns={columns}
+                dataSource={dataSetData}
+                pagination={{
+                  size: 'small',
+                  showSizeChanger: true,
+                }}
+                scroll={{ x: 'max-content' }}
+              />
+            </div>
+          </div>
         </div>
       </PageHeaderWrapper>
     );
--- src/pages/DatasetManagement/models/dataSet.js
+++ src/pages/DatasetManagement/models/dataSet.js
@@ -2,37 +2,52 @@
  * @Description: 获取数据集列表
  * @Author: lan
  * @Date: 2019-11-07 17:42:09
- * @LastEditTime: 2019-11-28 14:34:59
+ * @LastEditTime: 2019-11-28 17:48:27
  * @LastEditors: lan
  */
 // import { message } from 'antd';
 import Service from '@/utils/Service';
 
-const { getClassifyTree } = Service;
+const { getClassifyTree, getDataSet } = Service;
 
 export default {
   namespace: 'dataSet',
   state: {
     classifyTreeData: [], // 树列表
+    dataSetData: [], // 数据集
   },
   effects: {
-    // 获取数据集树
+    // 获取数据集分类树
     *getClassifyTree({ payload, callback }, { call, put }) {
       const response = yield call(getClassifyTree, { param: payload });
       if (response.bcjson.flag === '1') {
-        // 保存数据集树
+        // 保存数据集分类树
         yield put({
           type: 'setClassifyTreeData',
           payload: response.bcjson.items,
         });
         if (response.bcjson.items[0]) {
           // 默认选中第一个
-          //   yield put({
-          //     type: 'setActiveCID',
-          //     payload: response.bcjson.items[0].connectionId,
-          //   });
+          yield put({
+            type: 'getDataSet',
+            payload: {
+              taskId: response.bcjson.items[0].classId,
+              isShare: 0,
+            },
+          });
           if (callback) callback(response);
         }
+      }
+    },
+    // 获取数据集
+    *getDataSet({ payload }, { call, put }) {
+      const response = yield call(getDataSet, { param: payload });
+      if (response.bcjson.flag === '1') {
+        // 保存数据集
+        yield put({
+          type: 'saveDataSetData',
+          payload: response.bcjson.items,
+        });
       }
     },
   },
@@ -41,6 +56,12 @@ export default {
       return {
         ...state,
         classifyTreeData: action.payload,
+      };
+    },
+    saveDataSetData(state, action) {
+      return {
+        ...state,
+        dataSetData: action.payload,
       };
     },
   },
