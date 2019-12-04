5b420473832dbe688c0d3163d4f1735ce4afa0a9
liaozhiyong <liaozhiyong@szkingdom.com>
2019-12-04 16:59:39 +0800

    WorkflowAuditor

--- src/locales/zh-CN/menu.js
+++ src/locales/zh-CN/menu.js
@@ -52,6 +52,7 @@ export default {
   'menu.systemManagement.systemParameters': 'System Parameters',
   'menu.systemManagement.workflowConfig': 'Workflow Config',
   'menu.systemManagement.workflowDesign': 'Workflow Design',
+  'menu.systemManagement.ApprovalAuditor': 'Workflow Auditor',
   'menu.systemManagement.codeMaintenance': 'Code Maintenance',
   'menu.systemManagement.auditTrailLogging': 'Audit Trail Logging',
   'menu.systemManagement.scheduling': 'Scheduling Plan',
---
+++ src/pages/Approval/ApprovalAuditor/models/ApprovalAuditorModel.js
@@ -0,0 +1,107 @@
+import { message } from 'antd';
+import Service from '@/utils/Service';
+
+const { getAuditorlist, saveConfig, deployedModelList, getProcessResource, getQueryMenu } = Service;
+const ApprovalAuditorModel = {
+  namespace: 'ApprovalAuditor',
+  state: {
+    data: [],
+    deployedModelDatas: [],
+    processDefinitionId: '',
+    processName: '',
+    diagramDatas: '',
+    processImage: '',
+    roleGroupDatas: [],
+    auditorData: [],
+    GroupList: [],
+  },
+  effects: {
+    *saveConfigDatas({ payload, callback }, { call }) {
+      const response = yield call(saveConfig, { param: payload });
+      if (response.bcjson.flag === '1') {
+        message.success('保存成功');
+        callback({
+          pageNumber: '1',
+          pageSize: '10',
+        });
+      } else {
+        message.success('保存失败');
+      }
+    },
+    *getQueryMenuDatas({ payload }, { call, put }) {
+      const response = yield call(getQueryMenu, { param: payload });
+      if (response.bcjson.flag === '1') {
+        if (response.bcjson.items) {
+          yield put({
+            type: 'setMenuDatas',
+            payload: response.bcjson.items,
+          });
+        }
+      }
+    },
+    *getAuditorlistDatas({ payload }, { call, put }) {
+      const response = yield call(getAuditorlist, { param: payload });
+      if (response.bcjson.flag === '1') {
+        if (response.bcjson.items) {
+          yield put({
+            type: 'auditorlist',
+            payload: response.bcjson.items,
+          });
+        }
+      }
+    },
+    *deployedModelListDatas({ payload, callback }, { call, put }) {
+      const response = yield call(deployedModelList, { param: payload });
+      if (response.bcjson.flag === '1') {
+        if (response.bcjson.items) {
+          yield put({
+            type: 'deployedModelDatas',
+            payload: response.bcjson.items,
+          });
+          callback(response.bcjson.items[0].processDefinitionId);
+        }
+      }
+    },
+    *getProcessResourceDatas({ payload }, { call, put }) {
+      const response = yield call(getProcessResource, { param: payload });
+      if (response.bcjson.flag === '1') {
+        if (response.bcjson.items) {
+          yield put({
+            type: 'getImageUrl',
+            payload: response.bcjson.items,
+          });
+        }
+      }
+    },
+  },
+  reducers: {
+    deployedModelDatas(state, action) {
+      return {
+        ...state,
+        deployedModelDatas: action.payload,
+        processDefinitionId: action.payload[0].processDefinitionId,
+        processName: action.payload[0].name,
+      };
+    },
+    chooseProcessDefinitionId(state, action) {
+      return {
+        ...state,
+        processDefinitionId: action.payload.processDefinitionId,
+      };
+    },
+    setMenuDatas(state, action) {
+      const menuData = action.payload[0].MENU;
+      return {
+        ...state,
+        GroupList: menuData,
+      };
+    },
+    auditorlist(state, action) {
+      return {
+        ...state,
+        auditorData: action.payload,
+      };
+    },
+  },
+};
+export default ApprovalAuditorModel;
