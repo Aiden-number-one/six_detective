357e71f69a4ed57b4dea2c20e5fcbc38214fac65
lanjianyan@szkingdom.com <lanjianyan@szkingdom.com>
2019-12-07 13:23:19 +0800

    提交

---
+++ public/ETL/applications/common-page/single-database-change-info.js
@@ -0,0 +1,21 @@
+define(function(require, exports, module) {
+    var showContent = require("./single-database-change-infoShow");
+    var init = {
+        load: function() {
+            showContent._load(); //加载页面数据
+        }
+    };
+    $(function() {
+        // 点击查询
+        $("body").on("click", `#J_dcq_change_query_search`, function() {
+            showContent.getSingleDatasourceList();
+        });
+
+        $("body").on("click", "#J_dcq_change_single_query_table a", function() {
+            $("#J_sdci_checkmsg_modal").modal("show");
+            $("#J_sdci_checkmsg_modal_msg").html($(this).attr("checkmsg").replace(/\n/g, '<br>').replace(/\t/g, '&nbsp;&nbsp;'));
+        });
+
+    })
+    module.exports = init;
+})
