429ca382c44f5cb90aa8df8d6cc2e9767d06068d
mus <mus@szkingdom.com>
2019-12-07 18:18:10 +0800

    报表设计器

---
+++ src/pages/ReportDesigner/components/SideBar/LeftSideBar.jsx
@@ -0,0 +1,27 @@
+import React, { PureComponent } from 'react';
+import { Layout, Collapse, Icon, Form, Input, Select, Checkbox, Radio } from 'antd';
+import classNames from 'classnames';
+import { FormattedMessage } from 'umi/locale';
+import styles from './index.less';
+
+export default class LeftSideBar extends PureComponent {
+  state = {};
+
+  render() {
+    return (
+      <div className={classNames(styles.layout, styles.sideBar, styles.left)}>
+        <div className={styles.header}>
+          <div className={styles.title}>{<FormattedMessage id="report-designer.dataset" />}</div>
+        </div>
+        <div className={styles.datasetSelect}>
+          <Select className={styles.select} />
+          <div className={styles.icon}></div>
+        </div>
+      </div>
+    );
+  }
+}
+
+function ListItem() {
+  return <div className={classNames(styles.layout, styles.sideBar, styles.left)} />;
+}
---
+++ src/pages/ReportDesigner/locales/en-US.js
@@ -0,0 +1,31 @@
+/*
+ * @Des: 表格设计器国际化
+ * @Author: mus
+ * @Email: mus@szkingdom.com
+ * @Date: 2019-12-06 13:40:22
+ * @LastEditors: mus
+ * @LastEditTime: 2019-12-07 16:24:37
+ */
+
+export default {
+  'report-designer.widgetcontrol': 'Widget Control',
+  'report-designer.widgettype': 'Widget Type',
+  'report-designer.basic': 'Basic',
+  'report-designer.tag-name': 'Tag Name',
+  'report-designer.visible': 'Visible',
+  'report-designer.tooltip': 'Tooltip',
+  'report-designer.description': 'Description',
+  'report-designer.defaultstatus': 'Default Status',
+  'report-designer.normal': 'Normal',
+  'report-designer.readyonly': 'Readonly',
+  'report-designer.hide': 'Hide',
+  'report-designer.defaultvalue': 'Default Value',
+  'report-designer.field': 'Field',
+  'report-designer.option': 'Option',
+  'report-designer.from': 'From',
+  'report-designer.datasource': 'Data Source',
+  'report-designer.dataset': 'Data Set',
+  'report-designer.datacolumn': 'Data Column',
+  'report-designer.check': 'Check',
+  'report-designer.allownull': 'Allow Null',
+};
