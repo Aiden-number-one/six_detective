fcbba960c9adabcc85d1aeca4e97278c74d38e52
SeanLine <lines999000@163.com>
2019-12-14 11:33:29 +0800

    提交变革设计器相关

--- src/components/IconFont/index.js
+++ src/components/IconFont/index.js
@@ -2,7 +2,7 @@
  * @Description: iconfont
  * @Author: mus
  * @Date: 2019-08-13 17:14:11
- * @LastEditTime: 2019-12-12 17:46:53
+ * @LastEditTime: 2019-12-13 16:46:02
  * @LastEditors: mus
  * @Email: mus@szkingdom.com
  */
@@ -10,7 +10,7 @@ import { Icon } from 'antd';
 
 const IconFont = Icon.createFromIconfontCN({
   // scriptUrl: `http://${window.location.host}/iconfont.js`,
-  scriptUrl: '//at.alicdn.com/t/font_1472528_hviiu004l3q.js',
+  scriptUrl: '//at.alicdn.com/t/font_1472528_u8w6jqsotrb.js',
 });
 
 export default IconFont;
