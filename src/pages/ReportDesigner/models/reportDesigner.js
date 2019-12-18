bb8e197009a0f704ce3c1415992df35587325be9
SeanLine <lines999000@163.com>
2019-12-18 09:43:05 +0800

    提交报表设计器相关

--- src/pages/ReportDesigner/models/reportDesigner.js
+++ src/pages/ReportDesigner/models/reportDesigner.js
@@ -4,20 +4,70 @@
  * @Email: mus@szkingdom.com
  * @Date: 2019-12-02 16:36:09
  * @LastEditors: mus
- * @LastEditTime: 2019-12-12 13:31:15
+ * @LastEditTime: 2019-12-17 17:26:38
  */
 
-import fetch from '@/utils/request.default';
+import Service from '@/utils/Service';
+
+const { getDataSet, setTaskGridContent } = Service;
 
 export default {
   namespace: 'reportDesigner',
   state: {
     contentDetail: '', // 报表设计器的JSON
+    dataSetList: [], // 数据集列表
+    dataSetListColumn: [''], // 数据集的列
+    dataSetListSqlParams: [''], // 数据集的
+  },
+  reducers: {
+    // 设置数据集
+    setDataSetList(state, action) {
+      return {
+        ...state,
+        dataSetList: action.payload,
+      };
+    },
+    // 设置数据集的列
+    setDataSetColumn(state, action) {
+      return {
+        ...state,
+        dataSetListColumn: action.payload,
+      };
+    },
+    // 设置数据集的Sql Params
+    setDataSetSqlParams(state, action) {
+      return {
+        ...state,
+        dataSetListSqlParams: action.payload,
+      };
+    },
   },
   effects: {
+    // 获取数据集的列
+    *getDataSetColumn() {
+      //
+    },
+    // 获取数据集的Sql Params
+    *getDataSetSqlParams() {
+      //
+    },
+    // 获取数据集
+    *getDataSet({ payload }, { call, put }) {
+      const response = yield call(getDataSet, { param: payload });
+      let dataSetList = [];
+      if (response.bcjson.flag === '1') {
+        dataSetList = response.bcjson.items;
+      }
+      // 保存数据集
+      yield put({
+        type: 'setDataSetList',
+        payload: dataSetList,
+      });
+    },
+    // 新建表格任务
     *saveTaskGridContent(_, { call, select }) {
       const contentDetail = yield select(({ reportDesigner }) => reportDesigner.contentDetail);
-      const params = {
+      const param = {
         addSheetFormula: '',
         contentDetail,
         taskName: '新建表格',
@@ -25,8 +75,7 @@ export default {
         sheetInfo: '"[{"sheetName":"表格","sheetType":"0"}]"',
         isAllSave: '1',
       };
-      yield call(fetch('set_task_grid_content_save'), params);
+      yield call(setTaskGridContent, { param });
     },
   },
-  reducers: {},
 };
