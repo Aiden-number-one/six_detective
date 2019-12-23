f28a87d91ec26759c0567e0e337bdf41537a0565
dailinbo <dailinbo@szkingdom.com>
2019-12-23 15:48:44 +0800

    styles

--- src/app.js
+++ src/app.js
@@ -3,8 +3,8 @@
  * @Author: iron
  * @Email: chenggang@szkingdom.com.cn
  * @Date: 2019-11-07 17:09:07
- * @LastEditors: iron
- * @LastEditTime: 2019-12-18 14:44:17
+ * @LastEditors  : dailinbo
+ * @LastEditTime : 2019-12-23 15:37:43
  */
 
 import { message } from 'antd';
@@ -14,7 +14,7 @@ export const dva = {
     onError(e) {
       // if it's comment,component can not capture error
       e.preventDefault();
-      message.error(e.toString().slice(0, 100));
+      message.warning(e.toString().slice(0, 100));
     },
   },
 };
