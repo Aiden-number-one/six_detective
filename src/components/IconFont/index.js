d236f1ebad3ab711ad40b882c41803370c4a73f6
dailinbo <dailinbo@szkingdom.com>
2019-12-25 19:23:32 +0800

    icon

--- src/components/IconFont/index.js
+++ src/components/IconFont/index.js
@@ -2,7 +2,7 @@
  * @Description: iconfont
  * @Author: mus
  * @Date: 2019-08-13 17:14:11
- * @LastEditTime : 2019-12-25 17:00:47
+ * @LastEditTime : 2019-12-25 19:20:47
  * @LastEditors  : dailinbo
  * @Email: mus@szkingdom.com
  */
@@ -10,7 +10,7 @@ import { Icon } from 'antd';
 
 const IconFont = Icon.createFromIconfontCN({
   // scriptUrl: `http://${window.location.host}/iconfont.js`,
-  scriptUrl: '//at.alicdn.com/t/font_1472528_cwegtdwt0nt.js',
+  scriptUrl: '//at.alicdn.com/t/font_1472528_vljw7jeqixr.js',
 });
 
 export default IconFont;
