1e9d510435d0bb6e3f209b6bda960090bdbb4e42
SeanLine <lines999000@163.com>
2019-12-31 14:20:26 +0800

    提交报表设计器的相关代码

--- src/components/IconFont/index.js
+++ src/components/IconFont/index.js
@@ -2,15 +2,15 @@
  * @Description: iconfont
  * @Author: mus
  * @Date: 2019-08-13 17:14:11
- * @LastEditTime : 2019-12-27 19:15:17
- * @LastEditors  : dailinbo
+ * @LastEditTime : 2019-12-31 13:54:46
+ * @LastEditors  : mus
  * @Email: mus@szkingdom.com
  */
 import { Icon } from 'antd';
 
 const IconFont = Icon.createFromIconfontCN({
   // scriptUrl: `http://${window.location.host}/iconfont.js`,
-  scriptUrl: `//at.alicdn.com/t/font_1472528_sqhbglhvj7d.js?r=${Math.random()}`,
+  scriptUrl: `//at.alicdn.com/t/font_1472528_hvogztcz9vb.js?r=${Math.random()}`,
   // scriptUrl: '//at.alicdn.com/t/font_1472528_sqhbglhvj7d.js',
 });
 
