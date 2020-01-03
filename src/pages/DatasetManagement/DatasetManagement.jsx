188c210a644592f95622d5eb7c1ca473c76d0ec4
lanjianyan@szkingdom.com <lanjianyan@szkingdom.com>
2020-01-03 17:19:17 +0800

    数据集+fileType

--- src/pages/DatasetManagement/DatasetManagement.jsx
+++ src/pages/DatasetManagement/DatasetManagement.jsx
@@ -2,7 +2,7 @@
  * @Description: 数据集列表页面
  * @Author: lan
  * @Date: 2019-11-28 11:16:36
- * @LastEditTime : 2020-01-03 15:52:32
+ * @LastEditTime : 2020-01-03 17:16:27
  * @LastEditors  : lan
  */
 import React, { PureComponent } from 'react';
@@ -175,11 +175,11 @@ export default class DatasetManagement extends PureComponent {
   operateClassifyTree = values => {
     const { dispatch } = this.props;
     const param = {};
+    param.fileType = 'D';
     // 新增树节点
     if (this.operateType === 'ADD') {
       param.operType = this.operateType;
       param.isParentFolder = '1';
-      param.fileType = 'D';
       // 新增二级以下节点
       if (this.nodeTree) {
         param.folderName = values.folderName;
