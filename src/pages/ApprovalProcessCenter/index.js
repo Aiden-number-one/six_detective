cd100a04391d242c67334157001420b51937c511
liaozhiyong <liaozhiyong@szkingdom.com>
2020-01-15 20:47:32 +0800

    表头筛选移植

--- src/pages/ApprovalProcessCenter/index.js
+++ src/pages/ApprovalProcessCenter/index.js
@@ -6,17 +6,20 @@ import styles from './index.less';
 
 export default function() {
   const [task, setTask] = useState(null);
-  const [currentTaskType, setCurrentTaskType] = useState('all');
-  // const [alertItems, setAlertItems] = useState(null);
+  const [currentTaskType, setCurrentTaskType] = useState('SLOP_BIZ.V_ALL_TASK');
+  const [pageSizeData, setPageSizeData] = useState({});
 
   return (
     <div className={styles['list-container']}>
       <ProcessList
         getTask={item => setTask(item)}
         setCurrentTaskType={item => setCurrentTaskType(item)}
+        setPageSizeData={item => setPageSizeData(item)}
         // getAlertItems={items => setAlertItems(items)}
       />
-      {task && <ProcessDetail task={task} currentTaskType={currentTaskType} />}
+      {task && (
+        <ProcessDetail task={task} pageSizeData={pageSizeData} currentTaskType={currentTaskType} />
+      )}
       {/* <ProcessDetail task={task} currentTaskType={currentTaskType}/> */}
     </div>
   );
