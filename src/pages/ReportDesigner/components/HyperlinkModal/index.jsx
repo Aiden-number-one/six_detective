fa25ca142200383df76f8f5826dc8dc00cf01127
“liangchaoshun” <liangchaoshun@szkingdom.com>
2020-01-15 15:55:10 +0800

    重写了公式和超链接输入模态框和添加了异常提示信息

--- src/pages/ReportDesigner/components/HyperlinkModal/index.jsx
+++ src/pages/ReportDesigner/components/HyperlinkModal/index.jsx
@@ -2,15 +2,15 @@
  * @Author: liangchaoshun
  * @Email: liangchaoshun@szkingdom.com
  * @Date: 2020-01-13 20:28:07
- * @LastEditors  : mus
- * @LastEditTime : 2020-01-14 19:54:18
+ * @LastEditors  : liangchaoshun
+ * @LastEditTime : 2020-01-15 15:47:22
  * @Description: 超链接的模态框
  */
 
 import React, { PureComponent } from 'react';
 import { connect } from 'dva';
 
-import { Modal, Input } from 'antd';
+import { Form, Modal, Input } from 'antd';
 import { setCellTypeAndValue } from '../../utils';
 import less from './index.less';
 
@@ -18,24 +18,18 @@ import less from './index.less';
   showHylModal: reportDesigner.showHylModal,
   cellPosition: reportDesigner.cellPosition,
 }))
+@Form.create({ name: 'hyperlink_modal' })
 class HyperlinkModal extends PureComponent {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      hylContentValue: '', // 输入超文本要显示的内容以校验
-      hylLinkValue: '', // 输入超文本链接
-      hylEmptyVal: false, // 超文本内容为空，提示错误
-    };
-  }
-
   // 把超链接的 内容|地址 的初始值放到到 Input
   componentDidUpdate(prevProps) {
     const { initContentVal: prevContentVal, initLinkVal: prevLink } = prevProps;
     const { initContentVal: currContentVal, initLinkVal: currLink } = this.props;
     if (prevContentVal !== currContentVal || prevLink !== currLink) {
+      const {
+        form: { setFieldsValue },
+      } = this.props;
       // eslint-disable-next-line react/no-did-update-set-state
-      this.setState({ hylContentValue: currContentVal, hylLinkValue: currLink });
+      setFieldsValue({ hylContentValue: currContentVal, hylLinkValue: currLink });
     }
   }
 
@@ -50,53 +44,51 @@ class HyperlinkModal extends PureComponent {
 
   // 超链接模态框：确认
   hylConfirm = () => {
-    const { cellPosition, setCellStyle, dispatch } = this.props;
-    const { hylContentValue, hylLinkValue } = this.state;
-    if (hylContentValue.trim() === '') {
-      this.setState({ hylEmptyVal: true });
-      return;
-    }
-    this.setState({ hylEmptyVal: false });
-    setCellTypeAndValue({
-      type: 'hyperlink',
-      value: `${hylContentValue},${hylLinkValue}`,
+    const {
       cellPosition,
-    });
-    setCellStyle('underline', true); // 添加样式
-    setCellStyle('color', 'rgb(26,26,255)');
-    this.showOrHideHylModal(false);
-    this.setState({ hylContentValue: '', hylLinkValue: '' });
-    dispatch({
-      type: 'reportDesigner/modifyTemplateArea',
-      payload: {
-        elementType: 'link', // 单元格类型
-        link: hylLinkValue,
-      },
+      setCellStyle,
+      dispatch,
+      form: { validateFields, setFieldsValue },
+    } = this.props;
+    validateFields((err, values) => {
+      if (!err) {
+        // console.log('Received values of form: ', values);
+        const { hylContentValue, hylLinkValue } = values;
+        setCellTypeAndValue({
+          type: 'hyperlink',
+          value: `${hylContentValue},${hylLinkValue}`,
+          cellPosition,
+        });
+        setCellStyle('underline', true); // 添加样式
+        setCellStyle('color', 'rgb(26,26,255)'); // 添加颜色
+        this.showOrHideHylModal(false); // 隐藏 modal
+        setFieldsValue({ hylContentValue: '', hylLinkValue: '' }); // 清空
+        dispatch({
+          type: 'reportDesigner/modifyTemplateArea',
+          payload: {
+            elementType: 'link', // 单元格类型
+            link: hylLinkValue,
+          },
+        });
+      }
     });
   };
 
   // 超链接模态框：取消
   hylCancel = () => {
-    this.setState({ hylEmptyVal: false });
+    const {
+      form: { setFieldsValue },
+    } = this.props;
+    setFieldsValue({ hylContentValue: '', hylLinkValue: '' }); // 清空
+
     this.showOrHideHylModal(false);
   };
 
-  // 超链接：要显示的内容
-  hylContentChange = ev => {
-    const { value } = ev.target;
-    this.setState({ hylContentValue: value });
-  };
-
-  // 超链接：链接的地址
-  hylLinkChange = ev => {
-    const { value } = ev.target;
-    this.setState({ hylLinkValue: value });
-  };
-
   render() {
-    const { showHylModal } = this.props;
-    const { hylContentValue, hylLinkValue, hylEmptyVal } = this.state;
-
+    const {
+      showHylModal,
+      form: { getFieldDecorator },
+    } = this.props;
     return (
       <>
         <Modal
@@ -109,18 +101,34 @@ class HyperlinkModal extends PureComponent {
           onCancel={this.hylCancel}
           wrapClassName={less['hyperlink-modal']}
         >
-          <div>
-            <span>Display content:</span>
-            <Input
-              value={hylContentValue}
-              onChange={this.hylContentChange}
-              className={`${hylEmptyVal ? less['hyl-empty-error'] : ''}`}
-            />
-          </div>
-          <div className={less['link-addr']}>
-            <span>Link:</span>
-            <Input onChange={this.hylLinkChange} value={hylLinkValue} />
-          </div>
+          <Form layout="vertical">
+            <Form.Item label="Display content">
+              {getFieldDecorator('hylContentValue', {
+                validateFirst: true,
+                rules: [
+                  {
+                    required: true,
+                    message: 'Please input what you want to show',
+                  },
+                ],
+              })(<Input placeholder="Please input the content" />)}
+            </Form.Item>
+            <Form.Item label="Link">
+              {getFieldDecorator('hylLinkValue', {
+                validateFirst: true,
+                rules: [
+                  {
+                    required: true,
+                    message: 'Please input a link address',
+                  },
+                  {
+                    pattern: /^https?:\/\/(www\.)?.+\.com.*$/,
+                    message: 'Please input a',
+                  },
+                ],
+              })(<Input placeholder="Please start with http(s)://" />)}
+            </Form.Item>
+          </Form>
         </Modal>
       </>
     );
