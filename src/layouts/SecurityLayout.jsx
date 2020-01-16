54bbff88bea7e6de69e47df2f878d30320f023a7
dailinbo <dailinbo@szkingdom.com>
2020-01-16 10:36:09 +0800

    layout optimize

--- src/layouts/SecurityLayout.jsx
+++ src/layouts/SecurityLayout.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Redirect } from 'umi';
 import { connect } from 'dva';
+import { message } from 'antd';
+import router from 'umi/router';
 import { stringify } from 'querystring';
 import PageLoading from '@/components/PageLoading';
 import { getStore } from '@/utils/store';
@@ -8,6 +10,7 @@ import { getStore } from '@/utils/store';
 class SecurityLayout extends React.Component {
   state = {
     isReady: false,
+    loading: true,
   };
 
   componentDidMount() {
@@ -22,18 +25,60 @@ class SecurityLayout extends React.Component {
         // userAgent: window.navigator.userAgent,
       },
       callback: () => {},
+      logging: () => {
+        this.getMenu();
+      },
     });
   }
 
+  getMenu = () => {
+    const { dispatch, location } = this.props;
+    dispatch({
+      type: 'menu/getMenuData',
+      callback: (m, menuList) => {
+        // newMenuData = Object.assign([], menuData);
+        if (m.length <= 0) {
+          message.warning('The menu is empty');
+          dispatch({
+            type: 'login/logout',
+          });
+        }
+        if (!menuList.some(element => element.page.includes(location.pathname))) {
+          dispatch({
+            type: 'menu/getAdminMenuData',
+            payload: {},
+            callback: adminList => {
+              if (adminList.some(item => item.page.includes(location.pathname))) {
+                router.push('/no-access');
+                this.setState({
+                  loading: false,
+                });
+              } else {
+                router.push('/404');
+                this.setState({
+                  loading: false,
+                });
+              }
+            },
+          });
+        } else {
+          this.setState({
+            loading: false,
+          });
+        }
+      },
+    });
+  };
+
   render() {
-    const { isReady } = this.state;
+    const { isReady, loading } = this.state;
     const { children } = this.props;
     const isLogin = getStore('employeeId');
     const queryString = stringify({
       redirect: window.location.href,
     });
 
-    if (!isReady) {
+    if (!isReady || loading) {
       return <PageLoading />;
     }
     if (!isLogin) {
@@ -43,4 +88,7 @@ class SecurityLayout extends React.Component {
   }
 }
 
-export default connect(() => ({}))(SecurityLayout);
+export default connect(({ menu }) => ({
+  menuData: menu.menuData,
+  adminMenuData: menu.adminMenuData,
+}))(SecurityLayout);
