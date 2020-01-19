197f5493d428e819c1496f1c303aabaf3a080c10
SeanLine <lines999000@163.com>
2020-01-19 14:23:07 +0800

    提交iconfont相关

--- src/components/IconFont/index.js
+++ src/components/IconFont/index.js
@@ -2,15 +2,15 @@
  * @Description: iconfont
  * @Author: mus
  * @Date: 2019-08-13 17:14:11
- * @LastEditTime : 2020-01-16 17:54:32
- * @LastEditors  : lan
+ * @LastEditTime : 2020-01-19 14:18:38
+ * @LastEditors  : mus
  * @Email: mus@szkingdom.com
  */
 import { Icon } from 'antd';
 
 const IconFont = Icon.createFromIconfontCN({
-  // scriptUrl: `http://${window.location.host}/iconfont.js`,
-  scriptUrl: `//at.alicdn.com/t/font_1472528_flbqieq63zb.js?r=${Math.random()}`,
+  scriptUrl: `http://${window.location.host}/iconfont.js`,
+  // scriptUrl: `//at.alicdn.com/t/font_1472528_flbqieq63zb.js?r=${Math.random()}`,
   // scriptUrl: '//at.alicdn.com/t/font_1472528_sqhbglhvj7d.js',
 });
 
